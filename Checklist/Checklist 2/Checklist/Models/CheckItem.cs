e9a03d45ef46251a5afef11b3528211ff653bf64
hagru <213700@usn.no>
2021-09-26 20:31:01 +0200

    Initial commit

---
+++ Checklist/Checklist 2/Checklist/Models/CheckItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Checklist.Models
+{
+    public class CheckItem
+    {
+        public string Id { get; set; }
+        public string Owner { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Javascript/front-end/src/components/Card.js
@@ -0,0 +1,11 @@
+import './Card.css'
+
+function Card(){
+    return (
+        <div className="Card">
+            <h1>Random</h1>
+        </div>
+    )
+}
+
+export default Card;
